024c31230a3b7d89d79745168b4a50781365830c
sabrina-ulbrich <su@evolos.de>
2011-10-01 18:20:49 +0200

    add google analytics script tag

--- app/views/layouts/default.rb
+++ app/views/layouts/default.rb
@@ -9,6 +9,7 @@ class Layouts::Default < Layouts::Base
     end
 
     def javascripts(*names)
+      google_analytics
     end
 
     def header
@@ -55,6 +56,20 @@ class Layouts::Default < Layouts::Base
             s.src = 'http://' + disqus_shortname + '.disqus.com/count.js';
             (document.getElementsByTagName('HEAD')[0] || document.getElementsByTagName('BODY')[0]).appendChild(s);
         }());
+      js
+    end
+    
+    def google_analytics
+      script <<-js, :type => 'text/javascript'
+        var _gaq = _gaq || [];
+        _gaq.push(['_setAccount', 'UA-10149945-1']);
+        _gaq.push(['_trackPageview']);
+
+        (function() {
+          var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+          ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+          var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+        })();
       js
     end
 
